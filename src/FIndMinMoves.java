7ceffe8609f7e825d2c846e5f2c3b5fb11cb2c9f
Lakshmikanth Rao <lkanthrao@gmail.com>
2022-01-20 23:54:49 +0530

    [Leetcode] Find minimum number of moves

---
+++ src/FIndMinMoves.java
@@ -0,0 +1,33 @@
+public class FIndMinMoves {
+    public static void main(String[] args) {
+        minMoves(19, 2);
+    }
+    public static int minMoves(int target, int maxDoubles) {
+
+        int numberOfMoves = 0;
+
+        while(target > 1){ // 19,
+
+            if(maxDoubles == 0){// 2
+                numberOfMoves = numberOfMoves + (target - 1); // 2
+                return numberOfMoves;
+            }
+
+            if(maxDoubles > 0){// 2
+                target = target/2 + target%2; // 19/2 = 9, 8/2 = 4
+                numberOfMoves ++; // 1,
+                maxDoubles--;// 1,
+            }
+
+            if(target != 1 && target % 2 == 1){ // 8
+                target = target - 1; // 4
+                numberOfMoves++; // 2
+            }
+        }
+
+        return numberOfMoves;
+
+
+
+    }
+}
