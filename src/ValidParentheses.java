d1d9dfc969f77c54de7ddae3c750ddd6a91c49ae
lkanthrao <lkanthrao@gmail.com>
2023-11-01 21:12:12 +0530

    [Re-factored] refactored line diff.

---
+++ src/ValidParentheses.java
@@ -0,0 +1,14 @@
+/*
+    -   so just these char, any other occurance, return false
+    -   (){)(}] 
+
+*/
+class ValidParentheses {
+    public boolean isValid(String s) {
+
+        if (s == null || s.length() <= 1) return false;
+
+
+        return false;
+    }
+}
